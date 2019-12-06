fb21d1c8d9c891a860f58edd99e20422eabc62c9
Jimmique <jimmique88@gmail.com>
2019-12-06 14:11:20 -0800

    Add data-testid to gate and lock divs

--- src/display/Display.js
+++ src/display/Display.js
@@ -6,8 +6,14 @@ const Display = ({ closed, locked }) => {
 
   return (
     <div className="display panel" data-testid="display">
-      <div className={lockedClass}>{locked ? 'Locked' : 'Unlocked'}</div>
-      <div className={closedClass}>{closed ? 'Closed' : 'Open'}</div>
+      <div
+        className={lockedClass}
+        data-testid="display-lock"
+      >{locked ? 'Locked' : 'Unlocked'}</div>
+      <div
+        className={closedClass}
+        data-testid="display-gate"
+      >{closed ? 'Closed' : 'Open'}</div>
     </div>
   );
 };
