b7b7848cb923769e6ec1f5e52cad18537322a05c
Jimmique <jimmique88@gmail.com>
2019-12-06 04:45:44 -0800

    Added default Display test

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,11 @@
 // Test away!
+import React from 'react';
+import { render } from '@testing-library/react';
+import Display from './Display';
+
+test("it should default to 'unlocked' and 'open'", () => {
+    const { getByText } = render(<Display />);
+
+    expect(getByText(/unlocked/i));
+    expect(getByText(/open/i));
+});
